95f75cc917ed9f0fd7c086b859d31dcd69391ed2
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2016-12-15 11:40:07 +0000

    Working MPI version

--- multi_process_version.cpp
+++ multi_process_version.cpp
@@ -15,6 +15,15 @@
 // Define radius squared
 const float RADIUS_SQRT = RADIUS*RADIUS;
 
+//The SDL window
+SDL_Window* window = NULL;
+
+// Initialise renderer
+SDL_Renderer* renderer = NULL;
+
+// Start SDL event
+SDL_Event event;
+
 /*
  * Struct for each particle contains:
  *  - x
@@ -30,9 +39,14 @@ typedef struct {
      *  - get the comm size
      *  - make it so it splits particles into even chunks for each process
      *  - base process initalises random
-     *  - then does scatter to each process
-     *  - then each process does its batch of update?
-     *      - Means double loop - adds complexity
+     *
+     *  - then it does bcast to all processes so they know all other particles positions
+     *      - they still need to only update their amount
+     *      - potentially in the loop they know they're amount and they need to calculate their start and finish position
+     *  - once updated, they can do an allgather
+     *      - So root node can render all particles
+     *      - and all processes have the updated versions of the particles
+     *
      *  - then can split further and make multi threaded
      */
 
@@ -40,10 +54,21 @@ typedef struct {
 /**
  * Update the position and direction of particle based off of other particles
  */
-void updateParticles(particle_struct* particles) {
+particle_struct* updateParticles(particle_struct* particles, int particlesPerProcess, int myRank) {
+
+    // Calculate first and last index of this process's particles
+    int firstIndex = myRank * particlesPerProcess;
+    int lastIndex  = firstIndex + particlesPerProcess;
+
+    // Initialise counter for each processes' particles
+    int counter = 0;
+
+    // Create structure for particles
+    particle_struct* myParticles;
+    myParticles = (particle_struct*)malloc(sizeof(particle_struct)*particlesPerProcess);
 
     // Iterate all particles
-    for (int i = 0; i < NUM_PARTICLES; i++) {
+    for (int i = firstIndex; i < lastIndex; i++) {
 
         // Get particle to be updated by this iteration
         particle_struct particle = particles[i];
@@ -86,9 +111,23 @@ void updateParticles(particle_struct* particles) {
             particle.phi += DT*(COUPLING/near_count)*dphi;
         }
 
-        // Write new particle properties back to global memory.
-        particles[i] = particle;
+        // Write new particle properties
+        myParticles[counter] = particle;
+
+        counter = counter + 1;
     }
+
+    return myParticles;
+}
+
+void close() {
+
+    //Destroy window
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
 
 }
 
@@ -102,67 +141,92 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &commSize);
 
+    particle_struct* particles;
+    particle_struct* processParticles;
+    particles = (particle_struct*)malloc(sizeof(particle_struct)*NUM_PARTICLES);
+
+    // Check if root process
+    if (myRank == 0) {
+
+        // Initialise random positions and direction
+        for (int i = 0; i < NUM_PARTICLES; i++)
+        {
+            particles[i].x = (float)rand() / (float)RAND_MAX;
+            particles[i].y = (float)rand() / (float)RAND_MAX;
+            particles[i].phi = ((float)rand() / (float)RAND_MAX) * 2 * M_PI;
+        }
+        // Initialise window and check for error
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+            std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+        }
+
+        // Create window
+        window = SDL_CreateWindow("Vicsek",
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED,
+          SCREEN_W, SCREEN_H,
+          SDL_WINDOW_SHOWN);
+
+        // Check if window was created
+        if (window == NULL) {
+            // Error whilst creating
+            std::cout << "Error creating window: " << SDL_GetError() << std::endl;
+        }
+
+        // Initialise renderer
+        renderer = SDL_CreateRenderer(window, -1, 0);
+    }
+
+    // Broadcast the particles to all other processes
+    MPI_Bcast(particles, 3*NUM_PARTICLES, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
     // Calculate the how many particles each process should process
     int particlesPerProcess = NUM_PARTICLES / commSize;
 
-    std::cout << particlesPerProcess << std::endl;
+    // Initialise bool for quiting program
+    bool quit = false;
 
-    // Create structure for particles
-    particle_struct* particles;
-    particles = (particle_struct*)malloc(sizeof(particle_struct)*NUM_PARTICLES);
-
-    // Initialise random positions and direction
-    for (int i = 0; i < NUM_PARTICLES; i++)
+    while(!quit)
     {
-        particles[i].x = (float)rand() / (float)RAND_MAX;
-        particles[i].y = (float)rand() / (float)RAND_MAX;
-        particles[i].phi = ((float)rand() / (float)RAND_MAX) * 2 * M_PI;
-    }
+        //Handle events on queue 
+        while( SDL_PollEvent(&event) != 0 ) {
 
-    // Initialise window and check for error
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
-    }
+            //User requests quit
+            if(event.type == SDL_QUIT) {
+                quit = true;
+            }
 
-    // Create window
-    SDL_Window* window = SDL_CreateWindow("Vicsek",
-      SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      SCREEN_W, SCREEN_H,
-      SDL_WINDOW_SHOWN);
-
-    // Check if window was created
-    if (window == NULL) {
-        // Error whilst creating
-        std::cout << "Error creating window: " << SDL_GetError() << std::endl;
-    }
-
-    // Initialise renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-    // Start SDL event
-    SDL_Event event;
-    int running = 1;
-    while(running)
-    {
+        }
 
         // Update the particles
-        updateParticles(particles);
+        processParticles = updateParticles(particles, particlesPerProcess, myRank);
 
-        // Draw particles
-        SDL_RenderClear(renderer);
-        for (int i = 0; i < NUM_PARTICLES; i++)
-        {
-            SDL_RenderDrawLine(renderer,
-               particles[i].x*SCREEN_W,
-               particles[i].y*SCREEN_H,
-               particles[i].x*SCREEN_W + LINE_LEN*cos(particles[i].phi),
-               particles[i].y*SCREEN_H + LINE_LEN*sin(particles[i].phi));
+        // All gather so each process has a copy of the updated particles
+        MPI_Allgather(processParticles, 3*particlesPerProcess, MPI_FLOAT,
+            particles, 3*particlesPerProcess, MPI_FLOAT, MPI_COMM_WORLD);
+
+        // Check if root node
+        if (myRank == 0) {
+
+            // Draw particles
+            SDL_RenderClear(renderer);
+            for (int i = 0; i < NUM_PARTICLES; i++)
+            {
+                SDL_RenderDrawLine(renderer,
+                    particles[i].x*SCREEN_W,
+                    particles[i].y*SCREEN_H,
+                    particles[i].x*SCREEN_W + LINE_LEN*cos(particles[i].phi),
+                    particles[i].y*SCREEN_H + LINE_LEN*sin(particles[i].phi));
+
+            }
+
+            SDL_RenderPresent(renderer);
         }
-        SDL_RenderPresent(renderer);
 
     }
 
+    //Free resources and close SDL
+    close();
     MPI_Finalize();
     return 0;
 }
